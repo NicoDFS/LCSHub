2a7d96c4de2c744baf41597be0a7ed3a439e9168
Brad Bernard <bradbernard@me.com>
2014-07-12 19:36:02 -0700

    Changes

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => $_ENV['MYSQL_DATABSE'],
+			'username'  => $_ENV['MYSQL_USERNAME'],
+			'password'  => $_ENV['MYSQL_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => $_ENV['MYSQL_DATABSE'],
+			'username'  => $_ENV['MYSQL_USERNAME'],
+			'password'  => $_ENV['MYSQL_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
