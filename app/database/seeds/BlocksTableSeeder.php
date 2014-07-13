aa08c2a086f7ff7c8e390dc8eba9b8a5b6e1d5f5
Brad Bernard <bradbernard@me.com>
2014-07-13 10:58:46 -0700

    db

--- app/database/seeds/BlocksTableSeeder.php
+++ app/database/seeds/BlocksTableSeeder.php
@@ -8,31 +8,31 @@ class BlocksTableSeeder extends Seeder {
         Block::truncate();
 
         Block::create([
-            'blockId' => 1846,
-            'dateTime' => '2014-07-13T19:00:00+00:00',
-            'tickets' => '',
-            'leagueId' => 1,
-            'tournamentId' => 104,
-            'tournamentName' => 'NA Summer Split',
-            'significance' => 0,
-            'tbdTime' => 0,
-            'leagueColor' => '#1376A4',
-            'week' => 8,
-            'label' => 'NA Summer Split - Week 8 Day 2'
+            'blockId'           => 1846,
+            'dateTime'          => '2014-07-13T19:00:00+00:00',
+            'tickets'           => '',
+            'leagueId'          => 1,
+            'tournamentId'      => 104,
+            'tournamentName'    => 'NA Summer Split',
+            'significance'      => 0,
+            'tbdTime'           => 0,
+            'leagueColor'       => '#1376A4',
+            'week'              => 8,
+            'label'             => 'NA Summer Split - Week 8 Day 2'
         ]);
 
         Block::create([
-            'blockId' => 1847,
-            'dateTime' => '2014-07-19T19:00:00+00:00',
-            'tickets' => 'https://www.eventbrite.com/e/league-of-legends-lcs-na-summer-week-9-tickets-11487398117?ref=ebtn',
-            'leagueId' => 1,
-            'tournamentId' => 104,
-            'tournamentName' => 'NA Summer Split',
-            'significance' => 0,
-            'tbdTime' => 0,
-            'leagueColor' => '#1376A4',
-            'week' => 9,
-            'label' => 'NA Summer Split - Week 9 Day 1'
+            'blockId'           => 1847,
+            'dateTime'          => '2014-07-19T19:00:00+00:00',
+            'tickets'           => 'https://www.eventbrite.com/e/league-of-legends-lcs-na-summer-week-9-tickets-11487398117?ref=ebtn',
+            'leagueId'          => 1,
+            'tournamentId'      => 104,
+            'tournamentName'    => 'NA Summer Split',
+            'significance'      => 0,
+            'tbdTime'           => 0,
+            'leagueColor'       => '#1376A4',
+            'week'              => 9,
+            'label'             => 'NA Summer Split - Week 9 Day 1'
         ]);
     }
 
--- app/database/seeds/MatchesTableSeeder.php
+++ app/database/seeds/MatchesTableSeeder.php
@@ -8,17 +8,39 @@ class MatchesTableSeeder extends Seeder {
         Match::truncate();
 
         Match::create([
-            'blockId' => 1846,
-            'dateTime' => '2014-07-13T19:00:00+00:00',
-            'tickets' => '',
-            'leagueId' => 1,
-            'tournamentId' => 104,
-            'tournamentName' => 'NA Summer Split',
-            'significance' => 0,
-            'tbdTime' => 0,
-            'leagueColor' => '#1376A4',
-            'week' => 8,
-            'label' => 'NA Summer Split - Week 8 Day 2'
+            'dateTime'      => '2014-07-13T19:00Z',
+            'matchName'     => 'Curse vs Evil Geniuses',
+            'winnerId'      => '',
+            'matchId'       => 2593,
+            'url'           => '/tourney/match/2593',
+            'maxGames'      => 1,
+            'isLive'        => false,
+            'isFinished'    => false,
+            'liveStreams'   => false,
+            'polldaddyId'   => '8157255:37105063:37105064',
+
+            'tournamentId'      => 104,
+            'tournamentName'    => 'NA Summer Split',
+            'tournamentRound'   => 8,
+
+            'blueId'        => 4,
+            'blueName'      => 'Curse',
+            'blueLogoURL'   => '/sites/default/files/styles/grid_medium_square/public/CurseLogos_RGB.png?itok=U7znOoOr',
+            'blueAcronym'   => 'CRS',
+            'blueWins'      => 8,
+            'blueLosses'    => 11,
+
+            'redId'        => 426,
+            'redName'      => 'Evil Geniuses',
+            'redLogoURL'   => '/sites/default/files/styles/grid_medium_square/public/eglogo.png?itok=w5lGjHor',
+            'redAcronym'   => 'EG',
+            'redWins'      => 4,
+            'redLosses'    => 15,
+
+            'gameId'       => 3076,
+            'gameNoVods'   => false,
+            'gameHasVod'   => false,
+
         ]);
 
     }
