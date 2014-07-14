3eb0ec7efbb750622c58e19c50dab439a02d74a4
Brad Bernard <bradbernard@me.com>
2014-07-14 09:02:19 -0700

    yes

--- app/routes.php
+++ app/routes.php
@@ -11,13 +11,6 @@
 |
 */
 
-/*
-|   Route::get('/', function()
-|   {
-|	return View::make('hello');
-|   });
-*/
-
 Route::get('/block/{id}', function($id)
 {
 
@@ -29,24 +22,34 @@ Route::get('/block/{id}', function($id)
 Route::get('/today', function()
 {
 
-    $laTz = new DateTimeZone('America/Los_Angeles');
+    $timezone = 'America/Los_Angeles';
+    Cookie::queue('timezone', 'America/Louisville', (60 * 24));
 
-    $datetime = new DateTime();
-    $datetime->setTimezone($laTz);
+    if(Cookie::get('timezone'))
+    {
+        $timezone = Cookie::get('timezone');
+    }
 
-    $hoursLeft = 24 - $datetime->format('H');
-    $hoursUp = 24 - $hoursLeft;
+    $datetime = new DateTime('now', new DateTimeZone($timezone));
 
-    $query = "dateTime >= '" . date('Y-m-d H:i:s', strtotime("-" . $hoursUp . " hours")) . "' AND dateTime <= '" . date('Y-m-d H:i:s', strtotime("+" . $hoursLeft . " hours")) . "'";
+    $query = "dateTime >= '" . $datetime->format('Y-m-d') . " 00:00:00' AND dateTime <= '" . $datetime->format('Y-m-d') . " 23:59:59'";
     $todayBlock = Block::whereRaw($query)->first();
 
-    return View::make('today')->with('matches', $todayBlock->matches);
+    if(!is_null($todayBlock))
+    {
+        return View::make('today')->with('matches', $todayBlock->matches);
+    }
+    else
+    {
+        return View::make('none');
+    }
+
 });
 
 Route::get('/all', function()
 {
 
-    $allBlocks = Block::with('matches')->get();
+    $allBlocks = Block::with('matches')->orderBy('dateTime', 'DESC')->get();
 
     return View::make('all')->with('blocks', $allBlocks);
 });
@@ -54,13 +57,93 @@ Route::get('/all', function()
 Route::get('/insertblocks', function()
 {
 
-    $programmingUrl = 'http://na.lolesports.com/api/programming.json/?parameters[method]=prev&parameters[limit]=6&parameters[expand_matches]=1&parameters[tournament]=104';
-    $programming = json_decode(file_get_contents($programmingUrl));
+    $lcs = array('NA' => 104, 'EU' => 102);
+    $methods = array('prev', 'next');
 
     Eloquent::unguard();
 
-    foreach($programming as $program)
+    foreach($lcs as $region => $tournament)
     {
+
+        foreach($methods as $method)
+        {
+            $programmingUrl = 'http://na.lolesports.com/api/programming.json/?parameters[method]=' . $method . '&parameters[limit]=100&parameters[expand_matches]=1&parameters[tournament]=' . $tournament;
+            $programming = json_decode(file_get_contents($programmingUrl));
+
+            foreach($programming as $program)
+            {
+                $block = Block::firstOrCreate(['blockId' => $program->blockId]);
+
+                $block->update([
+                    'dateTime'          => date('Y-m-d H:i:s', strtotime($program->dateTime)),
+                    'tickets'           => $program->tickets,
+                    'leagueId'          => $program->leagueId,
+                    'tournamentId'      => $program->tournamentId,
+                    'tournamentName'    => $program->tournamentName,
+                    'significance'      => $program->significance,
+                    'tbdTime'           => $program->tbdTime,
+                    'leagueColor'       => $program->leagueColor,
+                    'week'              => $program->week,
+                    'label'             => $program->label,
+                    'bodyTime'          => date('Y-m-d H:i:s', strtotime($program->body[0]->bodyTime))
+                ]);
+
+                $block->save();
+
+                foreach($program->matches as $matchData)
+                {
+                    $match = Match::firstOrCreate(['matchId' => $matchData->matchId]);
+
+                    $match->update([
+                        'dateTime'          => date('Y-m-d H:i:s', strtotime($matchData->dateTime)),
+                        'matchName'         => $matchData->matchName,
+                        'winnerId'          => $matchData->winnerId,
+                        'url'               => $matchData->url,
+                        'maxGames'          => $matchData->maxGames,
+                        'isLive'            => $matchData->isLive,
+                        'isFinished'        => $matchData->isFinished,
+                        'liveStreams'       => $matchData->liveStreams,
+                        'polldaddyId'       => $matchData->polldaddyId,
+                        'blockId'           => $program->blockId,
+
+                        'tournamentId'      => $matchData->tournament->id,
+                        'tournamentName'    => $matchData->tournament->name,
+                        'tournamentRound'   => $matchData->tournament->round,
+
+                        'blueId'            => $matchData->contestants->blue->id,
+                        'blueName'          => $matchData->contestants->blue->name,
+                        'blueLogoURL'       => $matchData->contestants->blue->logoURL,
+                        'blueAcronym'       => $matchData->contestants->blue->acronym,
+                        'blueWins'          => $matchData->contestants->blue->wins,
+                        'blueLosses'        => $matchData->contestants->blue->losses,
+
+                        'redId'             => $matchData->contestants->red->id,
+                        'redName'           => $matchData->contestants->red->name,
+                        'redLogoURL'        => $matchData->contestants->red->logoURL,
+                        'redAcronym'        => $matchData->contestants->red->acronym,
+                        'redWins'           => $matchData->contestants->red->wins,
+                        'redLosses'         => $matchData->contestants->red->losses,
+
+                        'gameId'            => $matchData->gamesInfo->game0->id,
+                        'gameNoVods'        => $matchData->gamesInfo->game0->noVods,
+                        'gameHasVod'        => $matchData->gamesInfo->game0->hasVod,
+                    ]);
+
+                    $match->save();
+                }
+            }
+        }
+
+    }
+
+
+    $programIds = array('1826', '1827', '1828');
+
+    foreach($programIds as $pId)
+    {
+        $programmingUrl = 'http://na.lolesports.com:80/api/programming/' . $pId . '.json?expand_matches=1';
+        $program = json_decode(file_get_contents($programmingUrl));
+
         $block = Block::firstOrCreate(['blockId' => $program->blockId]);
 
         $block->update([
@@ -120,7 +203,7 @@ Route::get('/insertblocks', function()
 
             $match->save();
         }
-    }
 
+    }
 
 });
--- app/views/today.blade.php
+++ app/views/today.blade.php
@@ -18,7 +18,7 @@
             <ul class="list-group">
 
             @foreach($matches as $match)
-                <li class="list-group-item">{{  $match->matchName  }}
+                <li class="list-group-item">{{  $match->blueName  }} vs {{ $match->redName }}
                     @if ($match->isLive && !$match->isFinished)
                         <span class='pull-right'>Live</span>
                     @endif
