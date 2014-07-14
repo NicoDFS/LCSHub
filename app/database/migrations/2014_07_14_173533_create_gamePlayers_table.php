dc19670d784e0dd9a9fdc32e5fb6e73b92a50a6b
Brad Bernard <bradbernard@me.com>
2014-07-14 10:43:53 -0700

    read

--- app/database/migrations/2014_07_14_173533_create_gamePlayers_table.php
+++ app/database/migrations/2014_07_14_173533_create_gamePlayers_table.php
@@ -19,6 +19,36 @@ class CreateGamePlayersTable extends Migration {
 
                 $table->increments('id');
 
+                $table->integer('playerId');
+                $table->integer('teamId');
+                $table->integer('name');
+                $table->string('name');
+
+                $table->integer('championId');
+                $table->integer('endLevel');
+                $table->integer('kills');
+                $table->integer('deaths');
+                $table->integer('assists');
+                $table->double('kda', 5, 3);
+
+                $table->integer('item0Id');
+                $table->integer('item1Id');
+                $table->integer('item2Id');
+                $table->integer('item3Id');
+                $table->integer('item4Id');
+                $table->integer('item5Id');
+                $table->integer('item6Id');
+
+                $table->integer('spell0Id');
+                $table->integer('spell1Id');
+
+                $table->integer('totalGold');
+                $table->integer('minionsKilled');
+
+
+
+                $table->timestamps();
+
             });
 	}
 
