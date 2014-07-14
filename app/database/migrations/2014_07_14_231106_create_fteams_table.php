c6fac7ca7ddedbe7e0b98c4ec08392045191ef3b
Brad Bernard <bradbernard@me.com>
2014-07-14 16:16:45 -0700

    ya

---
+++ app/database/migrations/2014_07_14_231106_create_fteams_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFteamsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	    Schema::create('fTeams', function($table)
+            {
+
+                $table->engine = 'InnoDB';
+
+                $table->increments('id');
+                $table->integer('fId');
+                $table->integer('riotId');
+                $table->string('name');
+                $table->text('flavorText');
+                $table->string('positions');
+
+                $table->timestamps();
+
+            });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+	    Schema::drop('fTeams');
+	}
+
+}
---
+++ app/database/migrations/2014_07_14_231615_create_fteamsweek_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFteamsweekTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	    Schema::create('fTeamsWeek', function($table)
+            {
+
+                $table->engine = 'InnoDB';
+
+                $table->increments('id');
+
+
+                $table->timestamps();
+
+            });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+	    Schema::drop('fTeamsWeek');
+
+
+}
