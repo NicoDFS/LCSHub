18ab23b3e053228f023533f791faf20d57e2263b
Brad Bernard <bradbernard@me.com>
2014-07-14 16:30:57 -0700

    ya

---
+++ app/models/FTeam.php
@@ -0,0 +1,15 @@
+<?php
+
+class FTeam extends Eloquent {
+
+
+    protected $table = 'fTeam';
+    /*
+    public function players()
+    {
+        return $this->hasMany('GamePlayer', 'player', 'blockId');
+    }
+    */
+
+
+}
---
+++ app/models/FTeamWeek.php
@@ -0,0 +1,15 @@
+<?php
+
+class FTeamWeek extends Eloquent {
+
+
+    protected $table = 'fTeamWeek';
+    /*
+    public function players()
+    {
+        return $this->hasMany('GamePlayer', 'player', 'blockId');
+    }
+    */
+
+
+}
