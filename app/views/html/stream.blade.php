86b260a09bbba79ff8a14536cae919455bfe2e89
Brad Bernard <bradbernard@me.com>
2014-07-23 15:18:21 -0700

    ye

--- app/views/html/stream.blade.php
+++ app/views/html/stream.blade.php
@@ -10,7 +10,7 @@
     @if(isset($block->newMatchId))
         <iframe width="1280" height="720" src="//www.youtube-nocookie.com/embed/{{ $block->getMatches()[$block->gRequestedMatchIndex()]->game()->youtubeId() }}?autoplay=1&t=100000000&vq=hd1080" frameborder="0" allowfullscreen></iframe>
     @else
-         <iframe width="1280" height="720" src="//www.youtube-nocookie.com/embed/{{ $block->leagueYoutubeId() }}?autoplay=1&start=0&vq=hd1080" frameborder="0" allowfullscreen></iframe>
+         <iframe width="1280" height="720" src="//www.youtube-nocookie.com/embed/{{ $block->getMatches()[0]->game()->youtubeId() }}?autoplay=1&start=0&vq=hd1080" frameborder="0" allowfullscreen></iframe>
     @endif
 @elseif($block->isFutureBlock())
     <iframe width="1280" height="720" src="//www.youtube-nocookie.com/embed/{{ $block->leagueYoutubeId() }}?t=100000000&vq=hd1080" frameborder="0" allowfullscreen></iframe>
