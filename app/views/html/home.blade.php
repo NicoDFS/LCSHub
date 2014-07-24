e27f262a9816d844f08a090eb9c040af8e704c74
Brad Bernard <bradbernard@me.com>
2014-07-24 02:20:19 -0700

    ye

--- app/views/html/home.blade.php
+++ app/views/html/home.blade.php
@@ -40,23 +40,24 @@
 
         <script type="text/javascript">
             $(function() {
-                //setInterval(function()
-                //{
-                //    $.get("/ajax/refresh", function(data)
-                //    {
-                //        var obj = jQuery.parseJSON(data);
-                //        var scrl = $(document).scrollTop();
-                //
-                //        $("#scheduleBlock").html(obj.scheduleBlock);
-                //        $('body').scrollTop(scrl);
-                //        $("#pageHeader").html(obj.pageHeader);
-                //        //$("#streamContainer").html(obj.streamContainer);
-                //        $('.ttip, [data-toggle="tooltip"]').tooltip();
-                //        //$("#streamContainer").fitVids();
-                //
-                //    });
-                //
-                //}, 3000);
+                setInterval(function()
+                {
+                    $.get("/ajax/refresh", function(data)
+                    {
+                        var obj = jQuery.parseJSON(data);
+                        var scrl = $(document).scrollTop();
+
+                        $("#scheduleBlock").html(obj.scheduleBlock);
+                        $('body').scrollTop(scrl);
+                        $("#pageHeader").html(obj.pageHeader);
+                        //$("#streamContainer").html(obj.streamContainer);
+                        $('.ttip, [data-toggle="tooltip"]').tooltip();
+                        //$("#streamContainer").fitVids();
+
+                    });
+
+                }, 15000);
+
                 //
                 //$.get("/ajax/match/2515", function(data)
                 //{
