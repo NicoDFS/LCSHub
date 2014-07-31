ce615cf429c41e2a5ace523b55136bad9250d407
Brad Bernard <bradbernard@me.com>
2014-07-31 09:31:04 -0700

    ye

--- app/views/html/gamedetail.blade.php
+++ app/views/html/gamedetail.blade.php
@@ -1,7 +1,7 @@
 <?php $tblCount = 0; ?>
 @foreach($game->teams() as $teamKey => $team)
-<div class="table-responsive">
-<table style="{{ ($tblCount == 0 ? 'margin-bottom:20px' : '') }}" class="hover">
+<div class="table-responsive" style="{{ ($game->winnerId == $teamKey ? 'border:2px solid #60C060;' : '') }} {{ ($tblCount == 0 ? 'margin-bottom:20px' : '') }}">
+<table style="" class="hover">
         <thead>
                 <tr>
                         <th>Team: {{ ($teamKey == $game->blueId ? $game->blueName : $game->redName) }}</th>
@@ -28,11 +28,11 @@
         @foreach($team as $player)
                 <tr>
                         <td> <img class="img-rounded" style="border:1px solid black; width:70px; height:50px;" src="{{ $player->photoURL }}"> <span data-toggle="tooltip" title="Position: {{ $player->getFantasyPlayer()->role }}">{{ $player->name }}</span></td>
-                        <td class="text-center"> <img class="img-rounded" style="border:1px solid black;" src="http://lkimg.zamimg.com/shared/riot/images/champions/{{ $player->championId }}_32.png"> ({{ $player->endLevel }})</td>
+                        <td class="text-center"> <img class="img-rounded" style="border:1px solid black;" src="http://lkimg.zamimg.com/shared/riot/images/champions/{{ $player->championId }}_32.png"> &nbsp;({{ $player->endLevel }})</td>
                         <td class="text-center"><span data-toggle="tooltip" title="{{ floatval((double)$player->kda) }}">{{ $player->kills }}/{{ $player->deaths }}/{{ $player->assists }}</span></td>
                         <td class="text-center">{{ GamePlayer::count_format($player->totalGold) }}</td>
                         <td class="text-center">{{ $player->minionsKilled }}</td>
-                        <td class="text-center">{{ $player->getFantasyPlayer()->fantasyPoints() }}</td>
+                        <td class="text-center" style="font-weight:bold;">{{ $player->getFantasyPlayer()->fantasyPoints() }}</td>
                         <td class="text-center"> <img src="http://lkimg.zamimg.com/images/spells/{{ $player->spell0Id }}.png" style="width:32px; height:32px; border:1px solid black;" class="img-rounded"> <img src="http://lkimg.zamimg.com/images/spells/{{ $player->spell1Id }}.png" style="width:32px; height:32px; border:1px solid black;" class="img-rounded"> </td>
                         <td class="text-left">
                         @foreach($player->items() as $item)
@@ -47,8 +47,8 @@
 </table>
 </div>
 
-<div class="table-responsive">
-<table style="{{ ($tblCount == 0 ? 'margin-top: -10px; margin-bottom: 35px;' : ' margin-top:10px; margin-bottom:10px;') }}" class="hover">
+<div class="table-responsive" style="{{ ($tblCount == 0 ? 'margin-top: -10px; margin-bottom: 35px;' : ' margin-top:10px; margin-bottom:10px;') }} {{ ($game->winnerId == $teamKey ? 'border:2px solid #60C060;' : '') }}">
+<table style="" class="hover">
 
     <thead>
 
@@ -80,7 +80,7 @@
             <td class="text-center">{{ ($game->fantasyTeams()[$tblCount]->firstTower == 1 ? '<i class="fa fa-check"></i>' : '<i class="fa fa-times"></i>') }}</td>
             <td class="text-center">{{ ($game->fantasyTeams()[$tblCount]->firstInhibitor == 1 ? '<i class="fa fa-check"></i>' : '<i class="fa fa-times"></i>') }}</td>
             <td class="text-center">{{ $game->fantasyTeams()[$tblCount]->towersKilled }}</td>
-            <td class="text-center">{{ $game->fantasyTeams()[$tblCount]->fantasyPoints() }}</td>
+            <td class="text-center" style="font-weight:bold;">{{ $game->fantasyTeams()[$tblCount]->fantasyPoints() }}</td>
 
         </tr>
 
