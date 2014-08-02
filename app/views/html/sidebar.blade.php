ffeffb7bda8f6f78a8b5015894009056ca96ab17
Brad Bernard <bradbernard@me.com>
2014-08-02 12:08:06 -0700

    ye

--- app/views/html/sidebar.blade.php
+++ app/views/html/sidebar.blade.php
@@ -8,8 +8,14 @@
                   <div class="info">
                     <a href="#">Fantasy Stats Coming Soon</a>
                      <span>Soon (tm)</span>
+                        <br/>
+                        <p style="color:white; margin-top:30px;">
+                    LCShub.tv isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+                </p>
                   </div>
                 </div>
+
+
 
               </div>
             <div class="pane"><div class="slider" style="height: 266px; top: 238px;"></div></div></div>
