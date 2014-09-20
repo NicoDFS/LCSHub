dae52cfb184318f1bca31c0d7f80d6052d473a2b
Brad Bernard <bradbernard@me.com>
2014-09-20 02:22:43 -0700

    ye

--- app/views/html/game.blade.php
+++ app/views/html/game.blade.php
@@ -20,7 +20,7 @@
 
         @if(count($match->getGames()) > 1)
             <span class='label label-primary' style='font-size:16px;'>{{ $match->seriesResult(true) }}</span>
-        @elseif(count($match->getGames()) == 1 && $match->maxGames == 1)
+        @elseif(count($match->getGames()) == 1 && $match->maxGames == 1 && $match->isFinished == 1)
             <span class='label label-primary' style='font-size:16px;'>{{ ($match->getGames()[0]->gameLength/60) > 59  ? gmdate('G:i:s', $match->getGames()[0]->gameLength) : gmdate('i:s', $match->getGames()[0]->gameLength)  }}</span>
             @if($match->getGames()[0]->vodType != null)
                <span class='label label-danger' style='font-size:16px;'><span data-container='body' onclick="getGameVod('{{ $match->getGames()[0]->gameId }}');" style="cursor: pointer;" data-toggle="tooltip" data-placement="right" title="Watch Game"><i id='game-{{ $match->getGames()[0]->gameId }}-play' class="fa fa-youtube-play"></i></span></span>
